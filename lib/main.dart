6cbdb54b3b1c5bb4e9dbfa84322898bd3e1774e2
masanao10337 <ma-takahashi@croooober.co.jp>
2023-11-10 15:59:59 +0900

    add icon to the button

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Container(
-        decoration: BoxDecoration(
-          color: Colors.purple[700],
-        ),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 300,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  decoration: TextDecoration.none,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purpleAccent,
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Start Quiz',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 38, 6, 94),
+                Color.fromARGB(255, 116, 61, 212)
+              ],
+            ),
           ),
+          child: const StartScreen(),
         ),
       ),
     ),
